6eff2bb91e4a7c4966f635d44ad3c3b75c7298f2
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-07-23 13:06:38 +0530

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,11 @@
+// export default function Loading() {
+//   return null
+// }
+
 export default function Loading() {
-  return null
+  return (
+    <div className="flex items-center justify-center w-full h-full min-h-screen bg-white dark:bg-gray-900">
+      <div className="w-14 h-14 border-4 border-gray-200 border-t-purple-500 rounded-full animate-spin" />
+    </div>
+  )
 }
